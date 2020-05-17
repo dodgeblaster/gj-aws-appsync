95073bdbeedf4f4e25de3491d9b0e33a2362bbf7
Gary Jennings <garysjennings@gmail.com>
2020-05-16 22:27:02 -0600

    added tests

--- _testSetup/index.js
+++ _testSetup/index.js
@@ -1,12 +1,18 @@
 const iam = require('gj-aws-iam')
 const lambda = require('gj-aws-lambda')
+const dynamodb = require('gj-aws-dynamodb')
 const ACCOUNT_ID = '251256923172'
 
 
 const main = async () => {
     const srcLocation = __dirname + '/exampleProject/src'
     const zipLocation = __dirname + '/exampleProject/.serverless/code.zip'
+    
     const appsyncLambdaRole = 'int-test-appsynclambdarole'
+    const appsyncLambdaName = 'int-test-appsynclambda'
+    const dbName = 'int-test-appsyncdb'
+
+    console.log('setting up...')
 
     await iam.createRoleForLambda({
         state: false,
@@ -20,6 +26,14 @@ const main = async () => {
         handler: "index.handler",
         role: `arn:aws:iam::${ACCOUNT_ID}:role/${appsyncLambdaRole}`,
     })
+
+    await dynamodb.create({
+        name: dbName,
+        PK: 'id',
+        SK: false
+    })
+
+    console.log('done')
 
 }
 
